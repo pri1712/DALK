4dda983487e0f8c2faab389384422ef0ad612423
pri1712 <priyanshu171203@gmail.com>
2024-10-19 15:31:50 +0530

    Adding api relevant files

---
+++ LLM4KG/api_utils.py
@@ -0,0 +1,46 @@
+import time
+import openai
+from google.generativeai.types import HarmCategory, HarmBlockThreshold
+from google.api_core import retry
+palm.configure(api_key='')#replace this to your key
+api_key = ''#replace this to your key
+openai.api_key=api_key
+
+model = genai.GenerativeModel(model_name='gemini-1.5-flash')
+
+@retry.Retry()
+def request_api_gemini(messages):
+    completion = model.generate_content(
+        prompt=messages,
+            safety_settings=[
+            {
+                HarmCategory.HARM_CATEGORY_HATE_SPEECH: HarmBlockThreshold.BLOCK_NONE,
+                HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_NONE,
+                HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: HarmBlockThreshold.BLOCK_NONE,
+                HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_NONE,
+                HarmCategory. HARM_CATEGORY_CIVIC_INTEGRITY: HarmBlockThreshold.BLOCK_NONE,
+            }
+
+        ]
+    )
+    if len(completion.candidates) < 1:
+        print(completion)
+    ret = completion.candidates[0]['output']
+    return ret
+
+def request_api_chatgpt(prompt):
+    messages = [
+                {"role": "system", "content": 'You are an AI assistant to answer question about biomedicine.'},
+                {"role": "user", "content": prompt}
+    ]
+    try:
+        completion = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo", 
+            messages=messages,
+        )
+        ret = completion["choices"][0]["message"]["content"].strip()
+        return ret
+    except Exception as E:
+        time.sleep(2)
+        print(E)
+        return request_api_chatgpt(prompt)
